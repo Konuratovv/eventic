56bfaf46b9868bcb432f13c33a7e596508231669
Bekbolsun <bekbolsuntk@gmail.com>
2023-12-15 18:16:21 +0600

    poetry, toml, underscore of create_superuser are deleted and added permissions mixin

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from apps.users.managers import CustomUserManager
 
 
-class CustomUser(AbstractBaseUser):
+class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
