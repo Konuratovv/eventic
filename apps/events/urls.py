d683f59c191fa0915c344bf0b0e55bc59a73dc30
l9l9n_HP <l9l9ndos@gmail.com>
2023-12-21 03:36:09 +0600

    Added minor changes to view, urls and base

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 
-from .views import EventRetrieveAPIView, EventListAPIView, InterestsFilterEventAPIView
+from .views import EventRetrieveAPIView, EventListAPIView
 
 urlpatterns = [
     path("", EventListAPIView.as_view()),
     path("<int:pk>/", EventRetrieveAPIView.as_view()),
-    path("filter_interest/", InterestsFilterEventAPIView.as_view()),
-
 ]
--- apps/events/views.py
+++ apps/events/views.py
@@ -25,20 +25,29 @@ class EventListAPIView(generics.ListAPIView):
     """
     Вывод списка эвентов.
     Фильтрация по категориям.
-    Фильтрпция по дате евентов работает пока условно.
+    Фильтрация по дате евентов работает пока условно.
     """
     serializer_class = EventSerializer
 
     def get_queryset(self):
-        categories = self.request.query_params.get(
-            'categories', '')
+        categories = self.request.query_params.get('categories', '')
+        interests = self.request.query_params.get('interests', '')
+
         filters = (Q(permanentevent__id__gt=0) |
                    Q(temporaryevent__dates__end_date__lte=timezone.now()))
+
         if categories:
             category_filters = Q()
             for category in categories.split(','):
                 category_filters |= Q(category__name__exact=category)
             filters &= category_filters
+
+        if interests:
+            interest_filters = Q()
+            for interest in interests.split(','):
+                interest_filters |= Q(interests__name__exact=interest)
+            filters |= interest_filters
+
         return BaseEvent.objects.filter(filters).order_by('-id')
 
 
@@ -51,25 +60,3 @@ class FreeEventListAPIView(generics.ListAPIView):
 #     serializer_class = EventSerializer
 #     queryset = Event.objects.all().order_by()
 
-
-class InterestsFilterEventAPIView(generics.ListAPIView):
-    """
-    Фильтрация по 1 категорию.
-    Пример: http://127.0.0.1:8000/events/filter_cat/?category_name=Cat
-    И по нескольким категориям через запятую.
-    Пример: http://127.0.0.1:8000/events/filter_cat/?category_name=[cat, cat3]
-    """
-    serializer_class = EventSerializer
-
-    def get_queryset(self):
-        interests_name = self.request.query_params.get('interests_name', '')
-        interests_names_list = interests_name.strip('[]').split(',')
-
-        # Объект Q для каждой категории
-        q_objects = [Q(interests__name__iexact=interest.strip()) for interest in interests_names_list]
-
-        # Соединяем объекты Q с оператором ИЛИ
-        combined_q_objects = q_objects.pop() if q_objects else Q()
-        for q_object in q_objects:
-            combined_q_objects |= q_object
-        return BaseEvent.objects.filter(combined_q_objects)
