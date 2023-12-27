e9f4d75c36ac19d88a242fd4d57a810ae3cee635
Bekbolsun <bekbolsuntk@gmail.com>
2023-12-27 15:39:04 +0600

    t

--- apps/favourite/urls.py
+++ apps/favourite/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import EventFavoriteAPIView
 
 urlpatterns = [
-    path('api_favorite/', EventFavoriteAPIView.as_view(),name='favorite'),
+    path('api_favorite/', EventFavoriteAPIView.as_view({'post': 'create', 'delete': 'destroy', 'put': 'update'}),
+         name='favorite'),
 ]
