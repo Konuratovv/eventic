0f7278332931e3358ee1399aef7a5a93a21b2d63
l9l9n_HP <l9l9ndos@gmail.com>
2024-01-09 18:15:36 +0600

    Merge event type for event

--- apps/profiles/urls.py
+++ apps/profiles/urls.py
@@ -14,7 +14,6 @@ urlpatterns = [
     path('send_verify_code/', SendCodeAPIView.as_view()),
     path('verify/email/', VerifyAPIView.as_view()),
     path('follow/event/', FollowEventAPIView.as_view()),
-
     path('unfollow/event/', UnFollowEventAPIView.as_view()),
     path('send_reset_code/', SendResetAPiView.as_view()),
     path('check_reset_code/', CheckResetCodeAPIView.as_view()),
