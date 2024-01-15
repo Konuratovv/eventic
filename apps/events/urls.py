a74269d0907f2e0fdb69db3e3bf60901ef47bf66
l9l9n_HP <l9l9ndos@gmail.com>
2024-01-16 05:48:47 +0600

    Update filters and serializers, add permissions, create a new class EventTypeFilterAPIView

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -1,17 +1,14 @@
 from django.urls import path
 
 from .views import EventRetrieveAPIView, EventListAPIView, EventTypeListAPIView, EventDetailAPIView, \
-    EventCategoryListAPIView, EventInterestListAPIView
+    EventCategoryListAPIView, EventInterestListAPIView, EventTypeFilterAPIView
 
 urlpatterns = [
     path("", EventListAPIView.as_view()),
-    path("all_id/<int:pk>/", EventRetrieveAPIView.as_view()),
+    # path("all_id/<int:pk>/", EventRetrieveAPIView.as_view()),
     path("<int:pk>/", EventDetailAPIView.as_view()),
     path("category_list/", EventCategoryListAPIView.as_view()),
     path("interest_list/", EventInterestListAPIView.as_view()),
+    path("filter_event_type/", EventTypeFilterAPIView.as_view()),
     path('types/', EventTypeListAPIView.as_view()),
-
-    # Подписка и отписка от организатора в детейле
-    # path('organizer/<int:organizer_id>/follow/', FollowOrganizerView.as_view(), name='follow-organizer'),
-    # path('organizer/<int:organizer_id>/unfollow/', FollowOrganizerView.as_view(), name='unfollow-organizer'),
 ]
