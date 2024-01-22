eaaeb3ea0345d960baa7643f31e7b8decbbf7e01
Chyngyz <konuratov.chika@gmail.com>
2024-01-22 17:25:43 +0600

    Implemented the logic for displaying all events by type

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import EventRetrieveAPIView, EventListAPIView, EventDetailAPIView, \
     EventCategoryListAPIView, EventInterestListAPIView, EventTypeListAPIView, EventTypeFilterAPIView, \
-    FreeEventListAPIView
+    FreeEventListAPIView, AllEventsListAPIView, AllFreeEventsListAPIView, AllPaidEventsListAPIView, AllPermEventsListAPIView, AllTempEventsListAPIView
 
 urlpatterns = [
     path("", EventListAPIView.as_view()),
@@ -13,4 +13,9 @@ urlpatterns = [
     path("free_events_list/", FreeEventListAPIView.as_view()),
     path("filter_event_type/", EventTypeFilterAPIView.as_view()),
     path('types/', EventTypeListAPIView.as_view()),
+    path('types/all-events/', AllEventsListAPIView.as_view()),
+    path('types/free-events/', AllFreeEventsListAPIView.as_view()),
+    path('types/paid-events/', AllPaidEventsListAPIView.as_view()),
+    path('types/temporary-events/', AllTempEventsListAPIView.as_view()),
+    path('types/permanent-events/', AllPermEventsListAPIView.as_view()),
 ]
