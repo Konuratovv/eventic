20b4e5a14c10c0caafc906cc6affc175b9890faa
Chyngyz <konuratov.chika@gmail.com>
2024-02-05 17:51:19 +0600

    fixed ulrs

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import  EventListAPIView, EventDetailAPIView, \
     EventCategoryListAPIView, EventInterestListAPIView, EventTypeListAPIView, EventTypeFilterAPIView, \
-    FreeEventListAPIView
+    FreeEventListAPIView, AllEventsListAPIView, AllFreeEventsListAPIView, AllPaidEventsListAPIView, AllPermEventsListAPIView, AllTempEventsListAPIView
 
 urlpatterns = [
     path("", EventListAPIView.as_view()),
@@ -13,4 +13,10 @@ urlpatterns = [
     path("free_events_list/", FreeEventListAPIView.as_view()),
     path("filter_event_type/", EventTypeFilterAPIView.as_view()),
     path('types/', EventTypeListAPIView.as_view()),
+    path('types/all-events/', AllEventsListAPIView.as_view()),
+    path('types/all-free-events/', AllFreeEventsListAPIView.as_view()),
+    path('types/all-paid-events/', AllPaidEventsListAPIView.as_view()),
+    path('types/all-permanent-events/', AllPermEventsListAPIView.as_view()),
+    path('types/all-temporary-events/', AllTempEventsListAPIView.as_view()),
+    
 ]
