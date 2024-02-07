57719992a3b440cf89006f906fa609c2fe5805e2
Bekbolsun <bekbolsuntk@gmail.com>
2024-02-07 20:04:38 +0600

    region

--- apps/locations/serializers.py
+++ apps/locations/serializers.py
@@ -1,8 +1,25 @@
 from rest_framework import serializers
 
-from apps.locations.models import City
+from apps.locations.models import City, Region, Country
+
+
+class CountrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Country
+        fields = '__all__'
+
+
+class RegionSerializer(serializers.ModelSerializer):
+    country = CountrySerializer()
+
+    class Meta:
+        model = Region
+        fields = '__all__'
+
 
 class CitySerializer(serializers.ModelSerializer):
+    region = RegionSerializer()
+
     class Meta:
         model = City
         fields = '__all__'
