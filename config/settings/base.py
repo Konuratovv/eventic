2fd1373085273de8b122030709c8983c1122fea8
Bekbolsun <bekbolsuntk@gmail.com>
2024-02-09 16:48:22 +0600

    fixed event

--- config/settings/base.py
+++ config/settings/base.py
@@ -61,8 +61,8 @@ INSTALLED_APPS = [
     'django_extensions',
     'debug_toolbar',
     # 'cachalot',
-    'multiselectfield',
-    'django.contrib.postgres',
+    # 'multiselectfield',
+    # 'django.contrib.postgres',
 ]
 
 MIDDLEWARE = [
@@ -191,6 +191,6 @@ INTERNAL_IPS = [
 ]
 
 # settings.py
-CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
-CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379'
+# CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
+# CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379'
 
