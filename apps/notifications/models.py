bda90e8a3ecb917063312798b86f9c0fb4486b39
Aliaskar <alyas.toronov@mail.ru>
2024-02-12 23:02:21 +0600

    Notification models add

--- apps/notifications/models.py
+++ apps/notifications/models.py
@@ -1,24 +1,28 @@
 from django.db import models
 
+from apps.base.models import BaseModel
 
-class NotificationBase(models.Model):
-    user = models.ForeignKey('profiles.User', on_delete=models.CASCADE)
-    is_read = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    send_date = models.DateTimeField()
+
+class FollowBase(BaseModel):
+    user = models.ForeignKey('profiles.User', on_delete=models.CASCADE, related_name='follower')
 
     class Meta:
-        verbose_name = 'Уведомление'
-        verbose_name_plural = 'Уведомления'
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
 
 
-class NotificationPerm(NotificationBase):
-    event = models.ForeignKey('events.PermanentEventDays', on_delete=models.CASCADE)
+class FollowPerm(FollowBase):
+    event = models.ForeignKey('events.PermanentEventDays', on_delete=models.CASCADE, related_name='perm_event')
 
 
-class NotificationTemp(NotificationBase):
-    event = models.ForeignKey('events.EventDate', on_delete=models.CASCADE)
+class FollowTemp(FollowBase):
+    event = models.ForeignKey('events.EventDate', on_delete=models.CASCADE, related_name='temp_event')
 
 
-class NotificationOrg(NotificationBase):
-    organizer = models.ForeignKey('profiles.Organizer', on_delete=models.CASCADE)
+class FollowOrg(FollowBase):
+    organizer = models.ForeignKey('profiles.Organizer', on_delete=models.CASCADE, related_name='org_event')
+
+
+class Notification(BaseModel):
+    follow = models.ForeignKey(FollowBase, on_delete=models.CASCADE, related_name='notification')
+    is_seen = models.BooleanField(default=False)
